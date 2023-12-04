d8b31eb7ca3c3f3e1daadeabcc47d2075f5d7ed4
JeffTheStig <j.s.pronk@student.tudelft.nl>
2023-12-04 15:35:38 +0100

    add wav header files from stm32cube projects to platformio project

---
+++ Audiocat_PlatformIO_base/include/wav_header.h
@@ -0,0 +1,61 @@
+#include <stdint.h>
+
+typedef struct wav_header {
+    // RIFF Header
+    char riff_header[4]; // Contains "RIFF"
+    uint32_t wav_size; // Size of the wav portion of the file, which follows the first 8 bytes. File size - 8
+    char wave_header[4]; // Contains "WAVE"
+    
+    // Format Header
+    char fmt_header[4]; // Contains "fmt " (includes trailing space)
+    uint32_t fmt_chunk_size; // Should be 16 for PCM
+    uint16_t audio_format; // Should be 1 for PCM. 3 for IEEE Float
+    uint16_t num_channels;
+    uint32_t sample_rate;
+    uint32_t byte_rate; // Number of bytes per second. sample_rate * num_channels * Bytes Per Sample
+    uint16_t sample_alignment; // num_channels * Bytes Per Sample
+    uint16_t bit_depth; // Number of bits per sample
+    
+    // Data
+    char data_header[4]; // Contains "data"
+    uint32_t data_bytes; // Number of bytes in data. Number of samples * num_channels * sample byte size
+    // uint8_t bytes[]; // Remainder of wave file is bytes
+} wav_header;
+
+typedef struct bw_filter {
+	double b0;
+	double b1;
+	double b2;
+	double a0;
+	double a1;
+	double a2;
+
+	uint16_t y[3];
+	uint16_t x[3];
+} bw_filter;
+
+wav_header create_header(uint32_t wav_size, uint32_t sample_rate, uint32_t bytes_per_sample, uint16_t bit_depth, uint32_t fmt_chunk_size, uint16_t audio_fmt, uint16_t num_channels, uint32_t data_bytes);
+
+wav_header create_PCM_SC_header(uint32_t wav_size, uint32_t sample_rate, uint32_t bytes_per_sample, uint16_t bit_depth, uint32_t data_bytes);
+
+wav_header create_PCM_SC_header_correct(uint32_t num_samples);
+
+void endian_swap_32(uint32_t* dst, uint32_t src);
+
+void endian_swap_16(uint16_t* dst, uint16_t src);
+
+uint32_t endian_swap_32_ret(uint32_t src);
+
+uint16_t endian_swap_16_ret(uint16_t src);
+
+unsigned char* to_byte_array(wav_header header);
+
+bw_filter create_filter(double cutoff, double sample);
+
+uint16_t filter(bw_filter f, uint16_t input);
+
+#define endian_swap(dst, src)                         \
+_Generic((src),                                              \
+    uint32_t : endian_swap_32,          \
+    uint16_t : endian_swap_16)
+
